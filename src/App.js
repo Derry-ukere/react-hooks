c3aa9eab3df18cc7789ed0a25f84da69162db7fc
Derry Ukere <ukderry@gmail.com>
2021-02-24 22:58:13 +0100

    useState with arrays

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 // import FunctionalComponent from '../src/usetateHook/functionalComponent'
 // import FunctionalComponent from '../src/usetateHook/classComponent'
-import FunctionalComponent from '../src/usetateHook/counterThree'
+// import FunctionalComponent from '../src/usetateHook/counterThree'
+import FunctionalComponent from '../src/usetateHook/CounterFour'
 
 const App = () => {
   return (

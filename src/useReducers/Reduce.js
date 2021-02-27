406c673e56fc5021d50fb2a6714c2f1a868c6667
Derry Ukere <ukderry@gmail.com>
2021-02-27 12:16:41 +0100

    secnd counter

--- src/useReducers/Reduce.js
+++ src/useReducers/Reduce.js
@@ -1,14 +1,21 @@
 import React, { useReducer } from 'react'
-const initialState = 0
+const initialState = {
+  init: 0,
+  second: 100,
+}
 const reducer = (currentState, action) => {
   // returns new state
-  switch (action) {
+  switch (action.type) {
     case 'increase':
-      return currentState + 1
+      return { ...initialState, init: currentState.init + action.value }
     case 'decrease':
-      return currentState - 1
+      return { ...initialState, init: currentState.init - action.value }
+    case 'increase-sec':
+      return { ...initialState, second: currentState.second + action.value }
+    case 'decrease-sec':
+      return { ...initialState, second: currentState.second - action.value }
     case 'reset':
-      return initialState
+      return { ...initialState, init: initialState.init }
     default:
       return currentState
   }
@@ -16,16 +23,27 @@ const reducer = (currentState, action) => {
 
 const Reduce = () => {
   const [count, dispatch] = useReducer(reducer, initialState)
-  const increase = () => dispatch('increase')
-  const decrease = () => dispatch('decrease')
-  const reset = () => dispatch('reset')
+  const increase = () => dispatch({ type: 'increase', value: 1 })
+  const decrease = () => dispatch({ type: 'decrease', value: 1 })
+  const increaseFiv = () => dispatch({ type: 'increase', value: 5 })
+  const decreaseFiv = () => dispatch({ type: 'decrease', value: 5 })
+  const increaseSec = () => dispatch({ type: 'increase-sec', value: 1 })
+  const decreaseSec = () => dispatch({ type: 'decrease-sec', value: 1 })
+  const reset = () => dispatch({ type: 'reset' })
 
   return (
     <div>
-      <h5>count - {count} </h5>
+      <h5>count - {count.init} </h5>
+      <h6> second counter- {count.second}</h6>
       <button onClick={increase}>Increment</button>
       <button onClick={decrease}>decrement</button>
+      <button onClick={increaseFiv}>Increment 5</button>
+      <button onClick={decreaseFiv}>decrement 5</button>
       <button onClick={reset}>reset</button>
+      <div>
+        <button onClick={increaseSec}>Increment</button>
+        <button onClick={decreaseSec}>decrement</button>
+      </div>
     </div>
   )
 }

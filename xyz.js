f3bea6b968271cb4c4d246773803442407804b85
Derry Ukere <ukderry@gmail.com>
2021-02-28 08:47:49 +0100

    extra work

--- xyz.js
+++ xyz.js
@@ -1,40 +1,13 @@
-function sumFibs(num) {
-  return num
-}
-let r = sumFibs(4)
-// console.log(r)
-
-// fn = fn-1 + fn-2
-
-const countDown = (num) => {
-  if (num === 10) {
-    return
-  }
-  console.log(num)
-  countDown(num + 1)
-}
-
-function getPrimeNumbers(num) {
-  let store = []
-  for (let i = 0; i <= num; i++) {
-    if (i % 2 !== 0) {
-      store.push(i)
-    }
-  }
-  return store
-}
-const isPrime = (num) => {
-  for (let i = 2; i < num; i++) if (num % i === 0) return false
-  return num > 1
-}
-// function sumPrimes(num) {
-//   let primNumbers = isPrime(num)
-//   let reduceFunc = (store, current) => store + current
-//   return primNumbers.reduce(reduceFunc)
+// const btn = document.querySelector('.remove')
+// const handleClick = () => {
+//   console.log('clicked')
+//   btn.parentElement.remove()
 // }
 
-let r2 = isPrime(2)
-console.log(r2)
-//loop thru number
-// get prime numbers
-// sum up primenumbers
+// btn.onClick = handleClick
+
+const clickFunc = () => {
+  console.log('clicked')
+}
+
+document.getElementsByClassName('remove').addEventListener('click', clickFunc)
